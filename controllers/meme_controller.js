dd34e4290dcad0bf4a61d879dc30bd37c35aabd1
RyanMacc <arbader2142@gmail.com>
2022-04-27 15:27:21 -0400

    controller added for meme

---
+++ controllers/meme_controller.js
@@ -0,0 +1,59 @@
+//express require
+const express = require('express');
+// set up router
+const router = express.Router();
+
+/////////////////////// MODELS //////////////////////////
+
+const memes = require('../models/Meme.js')
+
+
+/////////////////////// ROUTES //////////////////////////
+
+// new get route -> http://localhost:4000/meme/new
+router.get('/new', (req, res) => {
+    res.render('new.ejs');
+});
+
+// show get route -> http://localhost:4000/meme/:id
+router.get('/:id', (req, res) =>{
+    const memeId = req.params.id;
+    const context = {oneMeme: memes[memeId], id: req.params.id};
+    res.render('show.ejs', context);
+});
+
+// edit get route -> http://localhost:4000/meme/:id/edit
+router.get('/:id/edit', (req,res) => {
+    const editMeme = memes[req.params.id];
+    const context = {editMeme: editMeme, id: req.params.id};
+    res.render('edit.ejs', context);
+});
+
+// meme index route -> http://localhost:4000/meme/
+router.get('/', (req, res) => {
+    const context = {memes:memes};
+    res.render('index.ejs', context);
+});
+
+// "create" post route -> http://localhost:4000/meme/
+router.post('/', (req,res) => {
+    memes.push (req.body);
+    res.redirect('/meme/');
+});
+
+// delete/destroy route -> http://localhost:4000/meme/:id
+router.delete('/:id', (req,res) => {
+    //need to change for backend
+    memes.splice(req.params.id, 1);
+    res.redirect('/meme/');
+});
+
+// update put route -> http://localhost:4000/meme/:id
+router.put('/:id', (req, res) => {
+    memes[req.params.id] = req.body;
+    res.redirect(`/meme/${req.params.id}`);
+});
+
+
+// export router
+module.exports = router;
--- server.js
+++ server.js
@@ -1,77 +1,45 @@
+/////////////////////// IMPORTS //////////////////////////
+
 // require npm express library 
 const express = require('express');
 // set up instance of express
 const app = express();
 // require method override library
 const methodOverride = require('method-override');
+// import meme_controller
+const memeController = require('./controllers/meme_controller.js');
 // set up port variable
 PORT = 4000;
 
-//models//
+/////////////////////// MODELS //////////////////////////
+
 // test Meme model to get initial routes working
 const memes = require('./models/Meme');
 
-//middleware//
+///////////////////MIDDLEWARE////////////////////////////
+
 //express static to find public folder/static css
 app.use(express.static('public'));
 // DELETE/UPDATE middle ware
 app.use(methodOverride('_method'));
 // body parser middleware
 app.use(express.urlencoded({extended:false}));
+// meme controller middleware
+app.use('/meme/', memeController);
 //application view engine to render ejs
 app.set('view engine', 'ejs');
 
-//routes//
-// new get route
-app.get('/meme/new', (req, res) => {
-    res.render('new.ejs');
-});
-
-// show get route
-app.get('/meme/:id', (req, res) =>{
-    const memeId = req.params.id;
-    const context = {oneMeme: memes[memeId], id: req.params.id};
-    res.render('show.ejs', context);
-});
-
-// edit route
-app.get('/meme/:id/edit', (req,res) => {
-    const editMeme = memes[req.params.id];
-    const context = {editMeme: editMeme, id: req.params.id}
-    res.render('edit.ejs', context);
-})
-
-// home index route 
-app.get('/meme/', (req, res) => {
-    const context = {memes:memes};
-    res.render('index.ejs', context);
-});
+/////////////////////// ROUTES //////////////////////////
 
 // home get route
 app.get('/', (req, res) => {
     res.send('Welcome to we meme it homepage');
 });
 
-// "create" post route
-app.post('/meme/', (req,res) => {
-    memes.push (req.body);
-    res.redirect('/meme/');
-});
 
-// delete/destroy route
-app.delete('/meme/:id', (req,res) => {
-    //need to change for backend
-    memes.splice(req.params.id, 1);
-    res.redirect('/meme/');
-});
-
-// update put route
-app.put('/meme/:id', (req, res) => {
-    memes[req.params.id] = req.body;
-    res.redirect(`/meme/${req.params.id}`);
-});
-
+//////////////////// CLIENT LISTEN ///////////////////////
 // app.listen to server at given port
 app.listen(PORT, () => {
     console.log(`listening on port: ${PORT}`);
 });
+
