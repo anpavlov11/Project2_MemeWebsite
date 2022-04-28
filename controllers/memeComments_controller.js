83fc7976d8a2e44d4cd8b3a5900500e9bfac0778
RyanMacc <arbader2142@gmail.com>
2022-04-28 11:51:04 -0400

    imported index.js with models

--- controllers/memeComments_controller.js
+++ controllers/memeComments_controller.js
@@ -1,7 +1,15 @@
+////////////////////////// IMPORTS //////////////////////////
+
 // import express methods
 const express = require('express');
 // creates instance of the router
 const router = express.Router();
+
+////////////////////////// MODELS //////////////////////////////
+
+// from models directory, need index.js for now till get rid of test
+const db = require('../models/index.js');
+
 
 // exports the router
 module.exports = router;
