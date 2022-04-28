8f12e910f47f374dd774af694b9914ed3ccaf89a
RyanMacc <arbader2142@gmail.com>
2022-04-28 11:48:10 -0400

    added memeComments controller with express boilerplate

---
+++ controllers/memeComments_controller.js
@@ -0,0 +1,7 @@
+// import express methods
+const express = require('express');
+// creates instance of the router
+const router = express.Router();
+
+// exports the router
+module.exports = router;
