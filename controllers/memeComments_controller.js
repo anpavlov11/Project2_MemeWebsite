1e89eb8fcd248f77569ee20f7b59f792cf26cb1b
RyanMacc <arbader2142@gmail.com>
2022-04-28 11:55:48 -0400

    commented out the routes for meme comment erd

--- controllers/memeComments_controller.js
+++ controllers/memeComments_controller.js
@@ -11,5 +11,22 @@ const router = express.Router();
 const db = require('../models/index.js');
 
 
+////////////////////////// ROUTES ///////////////////////////////
+
+// Index -GET- route for comments ERD, serves index.ejs template
+
+// New -GET- route for comments ERD, serves new.ejs template
+
+// Create -POST- route for comments ERD
+
+// Show route -GET- route for comments ERD, serves show.ejs template
+
+// Update route -PUT- route for comments ERD
+
+// Edit route -GET- route for comments ERD, serves edit.ejs template
+
+// Destroy route -DELETE- route for comments
+
+
 // exports the router
 module.exports = router;
